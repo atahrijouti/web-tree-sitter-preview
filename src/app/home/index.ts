d6f3edac547b2d6c8586de1e8c93c2c904b7ad86
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2025-04-03 12:29:55 +0200

    working web-tree-sitter + unbundle

--- src/app/home/index.ts
+++ src/app/home/index.ts
@@ -1,10 +1,25 @@
 import { html, type Metadata } from "unbundle"
+import { Language, Parser } from "web-tree-sitter"
 
 export const metadata: Metadata = {
-  title: "Tree-sitter web preview",
-  description: "A Tree-sitter theme previewer for your favorite text-editor",
+  title: "Tree sitter preview",
+  description: "Preview your tree-sitter theme",
+}
+
+export const ready = async () => {
+  await Parser.init({})
+  const parser = new Parser()
+  const JavaScript = await Language.load(
+    "/node_modules/tree-sitter-javascript/tree-sitter-javascript.wasm",
+  )
+  parser.setLanguage(JavaScript)
+  const sourceCode = "let x = 1; console.log(x);"
+  const tree = parser.parse(sourceCode)
+  console.log(tree)
 }
 
 export const content = () => {
-  return html`<div class="home-page">The Web Tree-Sitter Previewer will go here</div>`
+  return html`<div class="tree-sitter-page">
+    <h2>tree-sitter</h2>
+  </div>`
 }
